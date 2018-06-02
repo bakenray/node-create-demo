d1b903828f233c34677d685100264cdd041ea46d
bakenray <lpqstrong@163.com>
2018-06-02 17:10:09 +0800

    删改

--- demo.js
+++ demo.js
@@ -17,8 +17,8 @@ fs.mkdirSync('css')
 fs.mkdirSync('js') 
 
 fs.writeFileSync("./index.html", " <!DOCTYPE html>\n<head>\n<title>Hello</title>\n<link rel='stylesheet' href='css/style.css'>\n<script src='js/main.js'></script>\n</head>\n<body>\n<h1>Hi</h1>\n</body>")   //echo "" > index.html
-fs.writeFileSync("./css/style.css", "h1{color: red;}")  //echo "" > css/style.css
-fs.writeFileSync("./js/main.js", "var string = Hello World;\nalert(srting);")  //echo "" > js/main.js
+fs.writeFileSync("./css/style.css", "h1{color: red;}")
+fs.writeFileSync("./js/main.js", "var string = Hello World;\nalert(srting);")
 console.log("success")
 process.exit(0)  
 }
