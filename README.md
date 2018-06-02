88d7e23855744aa36ac0206d9358c63e97c216e8
bakenray <lpqstrong@163.com>
2018-06-02 16:54:44 +0800

    修改md

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # node-create-demo
+node.js 实现快速创建demo小脚本
---
+++ demo.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+var dirName = process.argv[2] 
+
+fs.exists(dirName,function(exists){
+
+if(exists){
+console.log("error: dir exists")
+process.exit(1)  
+}
+
+else{
+fs.mkdirSync("./" + dirName) 
+process.chdir("./" + dirName) 
+fs.mkdirSync('css') 
+fs.mkdirSync('js') 
+
+fs.writeFileSync("./index.html", " <!DOCTYPE html>\n<head>\n<title>Hello</title>\n<link rel='stylesheet' href='css/style.css'>\n<script src='js/main.js'></script>\n</head>\n<body>\n<h1>Hi</h1>\n</body>")   //echo "" > index.html
+fs.writeFileSync("./css/style.css", "h1{color: red;}")  //echo "" > css/style.css
+fs.writeFileSync("./js/main.js", "var string = Hello World;\nalert(srting);")  //echo "" > js/main.js
+console.log("success")
+process.exit(0)  
+}
+
+})
+
+
+
+
